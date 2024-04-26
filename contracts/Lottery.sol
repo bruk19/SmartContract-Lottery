8d8c904de6390a9f1946082dec5fcaf86318bd7d
Bruk Teshome <brukteshome21@gmail.com>
2024-04-26 16:42:11 +0300

    Import VRF for fulfill random

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,10 +1,12 @@
 //SPDX-License-Identiffier: MIT
-
 pragma solidity ^0.8.8;
+
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 error Raffle__NotEnoughETHEnered();
 
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2 {
    /* Staate Variables */
   uint256 private immutable i_entranceFee;
   Address payable[] private s_players;
@@ -21,10 +23,22 @@ contract Lottery {
     if(msg.value < i_entranceFee) {
       revert Raffle__NotEnoughETHEntered();
     }
+    s_players.push(payable(msg.sender));
+    emit raffleEnter(msg.sender);
   }
 
-  s_players.push(payable(msg.sender));
+  function pickRandomWinner() external {
+  }
 
-  emit raffleEnter(msg.sender);
+  function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
 
+  }
+
+  function getEntranceFee() public view returns (uint256) {
+    return i_entranceFee;
+  }
+
+  function getPlayer(uint256 index) public view returns (address) {
+    return s_players[index];
+  }
 }
