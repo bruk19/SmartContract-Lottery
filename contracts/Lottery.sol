fb09c5fdb80399df7d7721aae786d729a3a59958
Bruk Teshome <brukteshome21@gmail.com>
2024-04-26 15:32:43 +0300

    Emit event for raffle entered

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,16 +5,26 @@ pragma solidity ^0.8.8;
 error Raffle__NotEnoughETHEnered();
 
 contract Lottery {
+   /* Staate Variables */
   uint256 private immutable i_entranceFee;
+  Address payable[] private s_players;
+
+  /* Events */
+
+  event raffleEnter(address indexed player)
 
    constructor (uint256 entranceFee) {
     i_entranceFee = entranceFee;
   }
 
-  function enterRaffle() {
+  function enterRaffle() public payable {
     if(msg.value < i_entranceFee) {
       revert Raffle__NotEnoughETHEntered();
     }
   }
+
+  s_players.push(payable(msg.sender));
+
+  emit raffleEnter(msg.sender);
 
 }
