18373b5b51f61da1fa9fc52a923b6654a21baae4
Bruk Teshome <brukteshome21@gmail.com>
2024-04-29 15:04:18 +0300

    Add deploy implimentation for gasLane, callbackGasLimit and interval

--- deploy/1-deploy-raffle.js
+++ deploy/1-deploy-raffle.js
@@ -10,15 +10,27 @@ module.exports = async function ({getNamedAccounts, deployments }) {
   if (developmentChains.includes(network.name)) {
     const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
     vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address
+    /*subID */ 
+    const transactionResponse = await VRFCoordinatorV2Mock.createSubscription()
+    const transactionReceipt = await transactionResponse.wait(1)
+    subscriptionId = transactionReceipt.events[0].args.subId
+     
   } else {
     vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    subscriptionId = networkConfig[chainId]["subscriptionId"]
   }
 
-  const args = []
+  const entranceFee = networkConfig[chainId]["entranceFee"]
+  const gasLane = networkConfig[chainId]["gasLane"]
+  const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+  const interval = networkConfig[chainId]["interval"]
+
+  const args = [vrfCoordinatorV2Address, entranceFee, gasLane, subscriptionId, callbackGasLimit, interval]
   const Lottery = await deploy("Lottery", {
     from: deployer,
     args: args,
     log: true,
     waitConfirmations: network.config.blockConfirmations || 1,
   })
+
 }
