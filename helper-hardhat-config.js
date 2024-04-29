70b95e1085d96cb59bf85d3fb50a8c8fc452a7cb
Bruk Teshome <brukteshome21@gmail.com>
2024-04-29 14:02:46 +0300

    Add VRFCoordinatorV2Mock deployment

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,3 +4,10 @@ const networkConfig = {
     vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625"
   },
 }
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+  networkdConfig,
+  developmentChains,
+}
