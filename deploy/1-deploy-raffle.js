de5495c94cd45bb81c6125e8612226ab5ebe3a00
Bruk Teshome <brukteshome21@gmail.com>
2024-04-29 22:37:40 +0300

    Add getContractFactory for deployment

--- deploy/1-deploy-raffle.js
+++ deploy/1-deploy-raffle.js
@@ -2,22 +2,26 @@ const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 const FUND_AMOUNT = ethers.parseEther("1")
+const BASE_FEE = ethers.parseEther("0.25");
+const GAS_PRICE_LINK = 1e9
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
   const chainId = network.config.chainId
-  let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock
+  let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock, vrf_Coordinatorv2Mokck
 
   if (chainId == 31337) {
-    vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
-    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    vrfCoordinatorV2Mock = await ethers.getContractFactory("VRFCoordinatorV2Mock")
+    vrf_Coordinatorv2Mokck = await vrfCoordinatorV2Mock.deploy(BASE_FEE, GAS_PRICE_LINK)
+    console.log(vrf_Coordinatorv2Mokck.target)
+    // vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
     /*subID */
-    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
-    const transactionReceipt = await transactionResponse.wait()
-    subscriptionId = transactionReceipt.events[0].args.subId
+    const transactionResponse = await vrf_Coordinatorv2Mokck.createSubscription()
+    const transactionReceipt = await transactionResponse.wait(1)
+    subscriptionId = transactionReceipt.events[0].args.subId;
     //fund the subscription
-    await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
+    await vrf_Coordinatorv2Mokck.fundSubscription(subscriptionId, FUND_AMOUNT)
 
   } else {
     vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
