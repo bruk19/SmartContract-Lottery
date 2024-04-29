dcead423e787107f44c5f24a694191e7b8bc6df7
Bruk Teshome <brukteshome21@gmail.com>
2024-04-29 13:05:27 +0300

    Add network configration for sepolia network

--- deploy/1-deploy-raffle.js
+++ deploy/1-deploy-raffle.js
@@ -2,7 +2,7 @@ const { network } = require("hardhat")
 
 module.exports = async function ({getNamedAccounts, deployments }) {
   const { deploy, log } = deployments
-  const { deployer } = await getNamedAccounts
+  const { deployer } = await getNamedAccounts()
 
   const Lottery = await deploy("Lottery", {
     from: deployer,
