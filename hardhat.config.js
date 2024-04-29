ac10a043424c23a09a717697a33d086bef03a59f
Bruk Teshome <brukteshome21@gmail.com>
2024-04-29 12:27:42 +0300

    Update hardhat.config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+/** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-etherscan")
 require("hardhat-deploy")
@@ -8,5 +9,41 @@ require("dotenv").config()
 
 
 module.exports = {
+  defaultNetwork : "hardhat",
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    owner: {
+      default: 0,
+    },
+    funder1: {
+      default:  1,
+    },
+    funder2: {
+      default: 2
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+  },
+  mocha: {
+    timeout: 40000,
+  },
+  esm: {
+    nodeInterop: true,
+  },
 };
